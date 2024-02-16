d2aefa3135342b1b249803655e9024ff1ef3e916
현승범 <ghfk100@naver.com>
2024-02-16 18:58:40 +0900

    무한스크롤

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCoinsMarket } from "services/api";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+}
 
 function MainPage() {
+  const [coinList, setCoinList] = useState<Coin[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const fetchCoinList = async () => {
+    const newCoinsList = await getCoinsMarket(page);
+    setCoinList((prevList) => [...prevList, ...newCoinsList]);
+    if (newCoinsList.length === 0) {
+      setHasMore(false);
+    } else {
+      setPage(page + 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchCoinList();
+  }, []);
+
   return (
     <div>
-      <h1>Main Page</h1>
+      <h1>Welcome to BBEOM Coin Market!</h1>
+      <InfiniteScroll
+        dataLength={coinList.length}
+        next={fetchCoinList}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        {coinList.map((coin: Coin, index) => (
+          <div key={index}>
+            <h2>{coin.name}</h2>
+            <p>{coin.symbol}</p>
+          </div>
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
